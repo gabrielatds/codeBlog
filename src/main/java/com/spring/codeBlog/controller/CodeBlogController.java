7e3948c6b8ac844044b2b7143c2dc1eef70d4222
Gabriel Souza <gabriel.souza@targetwork.com.br>
2021-05-03 13:22:01 -0300

    projeto finalizado

--- src/main/java/com/spring/codeBlog/controller/CodeBlogController.java
+++ src/main/java/com/spring/codeBlog/controller/CodeBlogController.java
@@ -4,11 +4,15 @@ import com.spring.codeBlog.model.Post;
 import com.spring.codeBlog.service.CodeBlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -30,5 +34,22 @@ public class CodeBlogController {
         Post post = codeBlogService.FindById(id);
         mv.addObject("post", post);
         return mv;
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+    public String newPost(){
+        return "postForm";
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            return "redirect:/newpost";
+        }
+        else{
+            post.setData(LocalDate.now());
+            codeBlogService.Save(post);
+            return "redirect:/posts";
+        }
     }
 }
